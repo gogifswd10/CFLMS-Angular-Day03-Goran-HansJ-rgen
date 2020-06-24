ec5c967831828ba73bc2bf058e5c554f0cacae29
Goran Ivkić <65123282+gogifswd10@users.noreply.github.com>
2020-06-24 14:39:17 +0200

    angular day3

---
+++ angularProj/src/app/products.ts
@@ -0,0 +1,18 @@
+export const products = [
+ {
+   name: 'Corona-Maske XL Yves Saint Laurent',
+   price: 199,
+   description: 'Filtert alle Arten von Ungeziefer'
+ },
+ {
+   name: 'Kauknochen',
+   price: 2,
+   description: 'Ein Kauknochen'
+ },
+ {
+   name: 'Hundeleine aus echtem Tierleder',
+   price: 20,
+   description: 'Voll elastische Hundeleine'
+ }
+];
+
